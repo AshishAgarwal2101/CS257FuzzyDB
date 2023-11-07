d969fb060c393bbcd5cc8afe6f32bbcd191fdd2b
Ashish <ashish21.01agarwal@gmail.com>
2023-11-06 20:14:08 -0800

    SQL file

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2023 at 05:10 AM
+-- Generation Time: Nov 07, 2023 at 05:13 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -20,6 +20,53 @@ SET time_zone = "+00:00";
 --
 -- Database: `ecommerce`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `productid` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`productid`, `name`, `description`, `price`) VALUES
+('1111-aaaa', 'Ikea Chair 001', 'A wooden chair by IKEA', 250),
+('2222-aaaa', 'Milton Keyboard 005', 'Stay hydrated', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `sellerid` varchar(50) NOT NULL,
+  `sellername` varchar(50) NOT NULL,
+  `userid` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `productIds` varchar(400) NOT NULL,
+  `productNames` varchar(800) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `seller`
+--
+
+INSERT INTO `seller` (`sellerid`, `sellername`, `userid`, `username`, `productIds`, `productNames`, `rating`) VALUES
+('s1111', 'Blue Star', '', 'ashih', '[]', '[IKEA chir 001]', 4),
+('s2222', 'Cozy Cabin', '', 'mihiw', '[]', '[Miltn Keyboard 005]', 5),
+('s3333', 'Pressbox', '', 'parsh', '[]', '[]', 3),
+('s4444', 'IBM', '', 'ashifh', '', '[Google Nexus]', 5);
 
 -- --------------------------------------------------------
 
@@ -48,6 +95,18 @@ INSERT INTO `user` (`userid`, `username`, `email`, `password`, `age`, `wishlist`
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`productid`);
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`sellerid`);
 
 --
 -- Indexes for table `user`
